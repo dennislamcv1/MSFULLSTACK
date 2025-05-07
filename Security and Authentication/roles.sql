f3c49e1277aa0b74e5a87a285e30ceca0d8013f2
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2025-05-07 19:54:27 +0800

    Delete Security and Authentication directory

--- Security and Authentication/roles.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Users (
-    UserID INT PRIMARY KEY AUTO_INCREMENT,
-    Username VARCHAR(100) UNIQUE NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    Role ENUM('admin', 'user') NOT NULL DEFAULT 'user'  -- Add role field (admin/user)
-);
